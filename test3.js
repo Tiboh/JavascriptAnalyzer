171831b580f9895dfd9ddaa7532db8a504eacf8b
mouniamoun <mounia.hamedi@gmail.com>
2016-01-05 13:08:58 +0100

    update test3

--- test3.js
+++ test3.js
@@ -11,11 +11,11 @@ var int a
 a = 0
 switch (num)
 {
-	case 1: write("lunes"); break /* break in the same line */
-	/*case 2: write("martes")
-			break	/* break in other line non autorized*/
+	case 1: write("lunes") break /* break in the same line */
+	case 2: write("martes")
+			break	/* break in other line*/
 	case 3: write("miércoles")/* without break */
-	/*case 4:  write ("El dia numero ", num, " es Jueves .\n");  /* write with parameters*/
+	case 4:  write ("El dia numero ", num, " es Jueves .\n")  /* write with parameters*/
 	
 	case 8: if (a == 0) /*if without instructions */
 		{ 					
