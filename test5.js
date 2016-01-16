71dbfc337f97cb6182451432ac95349f42c9e2b7
mouniamoun <mounia.hamedi@gmail.com>
2016-01-16 12:09:12 +0100

    ajout test5

---
+++ test5.js
@@ -0,0 +1,26 @@
+/* PRUEBA 5 : INPUT OUTPUT TERMINAL */
+
+
+	function int Suma ( int a,  int b)  
+		{
+			var int c
+			c = a + b
+			return c
+		}
+
+/* OUTPUT TERMINAL */
+
+	var int entero1
+	var int entero2
+	var chars testChar
+	
+	write(entero1)
+	write(testChar)
+	
+	write(entero1+entero2)
+	write(Suma(entero1,entero2))
+	
+/* INPUT TERMINAL */
+	
+	prompt(entero1)
+	prompt(testChar)
